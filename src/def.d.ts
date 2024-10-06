70cbfbf9b649137542832e72e88dd3f5a02e7baa
marshift <marshift@riseup.net>
2024-10-06 16:52:05 +0100

    [Meta] Add and run various formatter thingies

--- src/def.d.ts
+++ src/def.d.ts
@@ -1,14 +1,14 @@
 declare module "@marshift/argus" {
-    interface ArgusContext {
-        argv: string[];
-        getPositionalArg: (position: number, required?: boolean) => string;
-        consumePositionalArg: (required?: boolean) => string;
-        hasOptionalArg: (condition: RegExp) => boolean;
-        getOptionalArg: (condition: RegExp) => string | undefined;
-    }
+	interface ArgusContext {
+		argv: string[];
+		getPositionalArg: (position: number, required?: boolean) => string;
+		consumePositionalArg: (required?: boolean) => string;
+		hasOptionalArg: (condition: RegExp) => boolean;
+		getOptionalArg: (condition: RegExp) => string | undefined;
+	}
 
-    const getPositionalArg: typeof import(".").getPositionalArg;
-    const hasOptionalArg: typeof import(".").hasOptionalArg;
-    const getOptionalArg: typeof import(".").getPositionalArg;
-    const createContext: typeof import(".").createContext;
+	const getPositionalArg: typeof import(".").getPositionalArg;
+	const hasOptionalArg: typeof import(".").hasOptionalArg;
+	const getOptionalArg: typeof import(".").getPositionalArg;
+	const createContext: typeof import(".").createContext;
 }
--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,22 @@
 import type { ArgusContext } from "@marshift/argus";
 
 /**
- * 
  * @param argv The arguments to parse
  * @param position The position to look up
  * @param required Whether this argument is required, default true
  * @returns The value of the positional argument
- * 
+ *
  * NOTE: This function filters out flags, be careful
  */
 export function getPositionalArg(argv: string[], position: number, required = true) {
-    const argvWithoutFlags = argv.filter((a) => !a.startsWith("-"));
-    const value = argvWithoutFlags[position];
-    if (!value && required) throw new Error(`Missing argument at position ${position}`);
+	const argvWithoutFlags = argv.filter((a) => !a.startsWith("-"));
+	const value = argvWithoutFlags[position];
+	if (!value && required) throw new Error(`Missing argument at position ${position}`);
 
-    return value;
+	return value;
 }
 
 /**
- * 
  * @param argv The arguments to parse
  * @param condition The expression to match the optional argument, e.g. `/--optional|-o/`
  * @returns Whether the given argument has been specified
@@ -30,39 +28,37 @@ export function getPositionalArg(argv: string[], position: number, required = tr
 export const hasOptionalArg = (argv: string[], condition: RegExp) => argv.some(i => condition.test(i));
 
 /**
- * 
  * @param argv The arguments to parse
  * @param condition The expression to match the optional argument, e.g. `/--optional|-o/`
  * @returns The value of the given argument
  */
 export function getOptionalArg(argv: string[], condition: RegExp) {
-    const argIdx = argv.findIndex(i => condition.test(i));
-    if (argIdx === -1) return;
+	const argIdx = argv.findIndex(i => condition.test(i));
+	if (argIdx === -1) return;
 
-    const splitArg = argv[argIdx].split("=").slice(0, 2);
-    if (splitArg.length === 1) throw new Error(`No value passed to argument ${argv[argIdx]}`);
+	const splitArg = argv[argIdx].split("=").slice(0, 2);
+	if (splitArg.length === 1) throw new Error(`No value passed to argument ${argv[argIdx]}`);
 
-    return splitArg[1];
+	return splitArg[1];
 }
 
 /**
- * 
- * @param argv The arguments to parse 
+ * @param argv The arguments to parse
  * @returns A "scoped" variant of the library's functions
  */
 export function createContext(argv: string[]): ArgusContext {
-    let _posArgIdx = 0;
-    
-    return {
-        argv,
-        getPositionalArg: (position, required) => getPositionalArg(argv, position, required),
-        consumePositionalArg: (required) => {
-            const value = getPositionalArg(argv, _posArgIdx, required);
-            
-            _posArgIdx++;
-            return value;
-        },
-        hasOptionalArg: (condition) => hasOptionalArg(argv, condition),
-        getOptionalArg: (condition) => getOptionalArg(argv, condition),
-    }
+	let _posArgIdx = 0;
+
+	return {
+		argv,
+		getPositionalArg: (position, required) => getPositionalArg(argv, position, required),
+		consumePositionalArg: (required) => {
+			const value = getPositionalArg(argv, _posArgIdx, required);
+
+			_posArgIdx++;
+			return value;
+		},
+		hasOptionalArg: (condition) => hasOptionalArg(argv, condition),
+		getOptionalArg: (condition) => getOptionalArg(argv, condition),
+	};
 }
